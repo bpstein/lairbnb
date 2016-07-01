940d5a6cba0ca28889096f48b91f60021b882f80
Ben Stein <bumstain@gmail.com>
2016-07-01 23:10:44 +1000

    User can add photoan add photos to room listings

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'toastr-rails'
 gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'figaro'
+gem 'paperclip'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,6 +8,7 @@ class RoomsController < ApplicationController
   end
 
   def show
+    @photos = @room.photos
   end
 
   def new
@@ -17,18 +18,34 @@ class RoomsController < ApplicationController
   def create
     @room = current_user.rooms.build(room_params)
     if @room.save 
-      redirect_to @room, notice: "Room saved."
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      @photos = @room.photos
+      redirect_to edit_room_path(@room), notice: "Room saved."
     else
       render :new
     end
   end
 
   def edit
+    if current_user.id == @room.user.id
+      @photos = @room.photos
+    else
+      redirect_to root_path, notice: "You don't have aceess to that!"
+    end
   end
 
   def update
     if @room.update(room_params)
-      redirect_to @room, notice: "Room updated."
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      redirect_to edit_room_path(@room), notice: "Room updated."
     else
       render :edit 
     end
