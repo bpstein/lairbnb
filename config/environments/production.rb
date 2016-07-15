e495a500541d01239d38c9add5a2eb744820855c
Ben Stein <bumstain@gmail.com>
2016-07-16 00:06:03 +1000

    Updated S3 for image uploads in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-    # This sets paperclip to upload images to Amazon S3
+  # This sets paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
