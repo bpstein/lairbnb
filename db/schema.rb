52d043657a0fba20c3b7a1afcc8ad9f55162e925
anup <anup1710@gmail.com>
2018-03-04 17:03:37 +0530

    resolve the devise confirmable exception by adding confirmation columns in users table. - https://github.com/plataformatec/devise/wiki/How-To:-Add-:confirmable-to-Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716041856) do
+ActiveRecord::Schema.define(version: 20180304052439) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
@@ -108,6 +108,10 @@ ActiveRecord::Schema.define(version: 20160716041856) do
     t.string "image"
     t.string "phone_number"
     t.text "description"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
