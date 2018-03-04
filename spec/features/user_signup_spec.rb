70de333fdcb8e779fa7748580473e45f2f6b4107
anup <anup1710@gmail.com>
2018-03-04 17:23:25 +0530

    Resolve the user signup testcase by waiting for the response and checking the current path.

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -3,13 +3,13 @@ require 'web_helper'
 
 feature 'User can sign up' do 
 
-  scenario 'user can sign up' do 
+  scenario 'user can sign up' do
     visit '/'
     click_link('Sign Up')
     fill_in('Full Name', with: 'Mr User')
     fill_in('Email', with: 'user@email.com')
     fill_in('Password', with: 'bananas')
-    click_button('Sign up')  
-    expect(current_path).to eq '/'
+    click_button('Sign up')
+    wait_for { current_path }.to eq('/')
   end
 end
